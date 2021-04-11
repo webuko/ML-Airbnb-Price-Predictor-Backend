9b0382f46ab08e82646825fe886606b8a4791b3c
Michael Bucher <michael@axlabs.com>
2021-04-11 17:54:39 +0200

    #12: Set up an API mapping reading from a running mongodb

--- api/code/app.py
+++ api/code/app.py
@@ -1,22 +1,44 @@
-from flask import Flask, abort
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
-
+from bson.json_util import dumps
+from flaskr.model import *
 
 app = Flask(__name__)
 
-# db connection
+DOMAIN = "localhost"
+PORT = 27017
+
 client = MongoClient(
-    host='mongodb',
-    port=27017,
-    username='airbnb-user',
-    password='pass',
-    authSource='airbnb'
+    host = DOMAIN + ":" + str(PORT)
 )
+
+db = client["berlinAirbnb"]
+
+def to_json(data):
+    return dumps(data)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Hello everyone!'
 
+@app.route('/db-name')
+def name():
+    return db.name
+
+@app.route('/db-listings')
+def get_listings():
+    return to_json(db.listings_2019.find_one({"id": "1944"}))
+
+@app.route('/model')
+def prediction():
+    return jsonify(get_prediction(
+        request.form['latitude'],
+        request.form['longitude'],
+        request.form['property_type'],
+        request.form['room_type'],
+        request.form['bedrooms'],
+        request.form['guests_included'],
+        ))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
--- api/code/flaskr/model.py
+++ api/code/flaskr/model.py
@@ -1,2 +1,10 @@
+from decimal import *
+
 def load_model():
     return True
+
+def get_prediction(latitude, longitude, property_type, room_type, bedrooms, guests_included):
+    # todo: run model here
+
+    # this is a dummy return
+    return latitude
