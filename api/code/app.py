7cccc761d4ae29233bc5e30735f3b85ddb906588
Silvan Wehrli <silvanwehrli@Silvans-MacBook-Pro.local>
2021-05-12 18:24:02 +0200

    #21: add api call method for getting all listings

--- api/code/app.py
+++ api/code/app.py
@@ -1,16 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from pymongo import MongoClient
 from bson.json_util import dumps
 from flaskr.model import get_prediction
 
 
 app = Flask(__name__)
-
-'''DOMAIN = "localhost"
-PORT = 27017
-client = MongoClient(
-    host = DOMAIN + ":" + str(PORT),
-)'''
 
 client = MongoClient(host='mongodb',
                      username='airbnb-user', 
@@ -22,9 +16,39 @@ def to_json(data):
     return dumps(data)
 
 
-@app.route('/api/allListings')
+@app.route('/api/allListings', methods=['GET', 'POST'])
 def all_linstings():
-    return to_json(db.listings.find({}))
+    if request.method == 'POST':
+        if request.get_json():
+            if not isinstance(request.json, list):
+                abort(400, 'Request data must be provided as array of keys')
+            
+            keys_projection = {str(key): 1 for key in request.json}
+            keys_projection['_id'] = 0
+            keys_filter = {str(key): {"$exists": 1} for key in request.json}
+        else:
+            abort(400, 'Request data must have content-type application/json')
+    else:
+        keys_projection = {
+            '_id': 0,
+            'name': 1,
+            'price': 1,
+            'longitude': 1,
+            'latitude': 1,
+            'description': 1,
+            'picture_url': 1
+        }
+        keys_filter = {
+            'name': {'$exists': 1},
+            'price': {'$exists': 1},
+            'longitude': {'$exists': 1},
+            'latitude': {'$exists': 1},
+            'description': {'$exists': 1},
+            'picture_url': {'$exists': 1},
+        }
+    
+    return to_json(db.listings.find(keys_filter, keys_projection))
+
 
 '''@app.route('/')
 def hello():
