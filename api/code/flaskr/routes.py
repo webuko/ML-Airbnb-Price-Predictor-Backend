c9efa9fd6c60e84e78ce2d269c1a47268f5b6e19
Silvan Wehrli <wehrsi@bluewin.ch>
2021-05-29 16:29:21 +0200

    added ref to swagger subpage in source code

--- api/code/flaskr/routes.py
+++ api/code/flaskr/routes.py
@@ -18,8 +18,9 @@ CORS(api_bp)
 def all_listings():
     """Endpoint for retrieving all listings.
 
-        For an explanation on how to use the api check out our API documentation on github:
-        https://github.com/webuko/backend/wiki/API-Documentation#alllistings
+    For an explanation on how to use the api check out our API documentation on github:
+    https://github.com/webuko/backend/wiki/API-Documentation#alllistings
+    or visit the subpage /apidocs.
     """
 
     projected_request = project_listings(request)
@@ -43,6 +44,7 @@ def filtered_listings():
 
     For an explanation on how to use the api check out our API documentation on github:
     https://github.com/webuko/backend/wiki/API-Documentation#filterlistings
+    or visit the subpage /apidocs.
     """
 
     validated_request = validate_filter_request(request)
@@ -72,7 +74,8 @@ def price_prediction():
     """Endpoint for making a price prediction request.
 
     For an explanation on how to use the api check out our API documentation on github:
-    https://github.com/webuko/backend/wiki/API-Documentation#priceprediction
+    https://github.com/webuko/backend/wiki/API-Documentation#priceprediction 
+    or visit the subpage /apidocs.
     """
 
     validated_request = validate_prediction_request(request)
@@ -99,6 +102,7 @@ def price_prediction_param_values():
 
     For an explanation on how to use the api check out our API documentation on github:
     https://github.com/webuko/backend/wiki/API-Documentation#pricepredictionparamvalues
+    or visit the subpage /apidocs.
     """
 
     param_values = allowed_prediction_features()
@@ -116,6 +120,7 @@ def avg_price_neighbourhood():
     """Endpoint for retrieving the average price per neighbourhood (including geojson data).
         For an explanation on how to use the api check out our API documentation on github:
         https://github.com/webuko/backend/wiki/API-Documentation#avgpriceperneighbourhood
+        or visit the subpage /apidocs.
     """
 
     pipeline = [
